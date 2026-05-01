c7cf77f85a784525a9a64fc38fadb2ef8e38ae0b
gauravbarge <gauravbarge@live.com>
2026-05-01 16:43:40 -0400

    Initial Commit boilerplate

---
+++ src/WebApp/src/App.tsx
@@ -0,0 +1,19 @@
+// src/WebApp/src/App.tsx
+
+function App() {
+  return (
+    <div style={{
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      height: '100vh', 
+      backgroundColor: '#282c34', 
+      color: 'white', 
+      fontSize: '2rem'
+    }}>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
